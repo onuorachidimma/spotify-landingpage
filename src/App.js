f4585556b451c9093be8f17f0b217e663d474932
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-08-04 12:07:21 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import SpotifyLandingPage from './components/spotifyLandingPage';
+function App() {
+  return (
+    <div className="App">
+      <SpotifyLandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/spotifyLandingPage.jsx
@@ -0,0 +1,9 @@
+const SpotifyLandingPage = () => {
+    return(
+        <div>
+        Spotify LandingPage
+        </div>
+    )
+}
+
+export default SpotifyLandingPage
